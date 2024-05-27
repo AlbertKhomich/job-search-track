0eba90fe14f3209f21d58108226ec1411954be85
admin <admin@Albert-Khomich.local>
2024-05-27 09:23:05 +0200

    minor improvements

--- app/ui/create-form.tsx
+++ app/ui/create-form.tsx
@@ -6,6 +6,7 @@ import { Datepicker } from "flowbite-react";
 import { useFormState } from "react-dom";
 import { Badge } from "flowbite-react";
 import SaveButton from "./save-button";
+import { useEffect, useRef } from "react";
 
 export const statuses = [
   "Beworben",
@@ -19,6 +20,13 @@ export const colors = ["gray", "failure", "warning", "success"];
 export default function AddForm() {
   const initialState = { message: "", errors: {} };
   const [state, dispatch] = useFormState(addCompany, initialState);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
 
   return (
     <Card className="max-w-sm w-full">
@@ -32,6 +40,7 @@ export default function AddForm() {
             name="companyName"
             type="text"
             aria-describedby="company-error"
+            ref={inputRef}
           />
           <div id="company-error" aria-live="polite" aria-atomic="true">
             {state.errors?.name &&
